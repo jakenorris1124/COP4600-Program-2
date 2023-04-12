46e5ac0f62f29cbcd8f73b4ad410a9689ab50459
Jake Norris <jakenorris1124@gmail.com>
2023-04-12 15:56:54 -0400

    Replaced old test with new test

---
+++ test.py
@@ -0,0 +1,34 @@
+from threading import Thread
+import os
+import time
+
+def readDevice(loopCount, delay):
+    print("Reading from Device")
+    for x in range(loopCount):
+        device = open("/dev/charkmod-out", "r")
+        fileContents = device.read()
+        device.close()
+        time.sleep(delay/1000)
+
+def writeDevice(inputString, loopCount, delay):
+    print("Writing to Device")
+    for x in range(loopCount):
+        device = open("/dev/charkmod-in", "w")
+        device.write(inputString)
+        device.close()
+        time.sleep(delay/1000)
+
+def main():
+    writerThread = Thread(target=writeDevice, args=("Hello", 50, 10))
+    readerThread = Thread(target=readDevice, args=(50,10))
+
+    writerThread.start()
+    readerThread.start()
+
+    writerThread.join()
+    readerThread.join()
+
+    print("Done!")
+
+if __name__ == "__main__":
+    main()
